5934f6ab832ca59963d02aee7f0d170d1e02276b
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2025-11-11 17:55:40 -0300

    closes #4 -  Implementa funcionalidade de historico de pesquisas dinamico

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,3 +1,5 @@
+var usersHistory = [];
+
 function search() {
 
     var username = document.getElementById("inputUserName").value;
@@ -5,13 +7,37 @@ function search() {
     var url = `https://api.github.com/users/${username}`;
 
     $.getJSON(url, (user) => {
-        clearError();
         showUserData(user);
+
+        if (isNew(user)) {
+            saveUser(user);
+            showNewUserHistory(user);
+        }
+
+        clearError();
     }).fail(() => {
         showUserData({});
         showError("Não Encontrado!");
     });
 
+}
+
+function saveUser(user) {
+    usersHistory.push(user);
+}
+
+function isNew(user) {
+    return usersHistory.filter((u) => u.login === user.login).length == 0;
+}
+
+function showNewUserHistory(user) {
+
+    document.getElementById("history").innerHTML += `
+                 <div class="col">
+                 <img src=${user.avatar_url} width="110"
+                 height="110" class="shadow rounded">
+                </div>
+    `
 }
 
 function showError(msg) {
@@ -23,15 +49,15 @@ function clearError() {
     document.getElementById("error").innerHTML = "";
 }
 
-function showUserData() {
+function showUserData(user) {
     document.getElementById("name").innerHTML = user.name || "";
     document.getElementById("html_url").innerHTML = user.html_url || "";
     document.getElementById("company").innerHTML = user.company || "";
-    document.getElementById("avatar_url").innerHTML = user.avatar_url ? 
-                                                 `
+    document.getElementById("avatar_url").innerHTML = user.avatar_url ?
+        `
                                                  <img src="${user.avatar_url}" width="220" height="220"
                                                  class="shadow rounded"></img>
-                                                 ` : 
-                                                 "";
+                                                 ` :
+        "";
 
 }
